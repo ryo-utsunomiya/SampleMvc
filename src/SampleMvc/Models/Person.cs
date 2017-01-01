06327e5e7589d698d8d5a0491069b3c757264461
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-01 15:51:29 +0900

    Modify People/Index page

--- src/SampleMvc/Models/Person.cs
+++ src/SampleMvc/Models/Person.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleMvc.Models
 {
     public partial class Person
     {
         public int Id { get; set; }
+        [Display(Name = "名前")]
         public string Name { get; set; }
+        [Display(Name = "年齢")]
+        [DisplayFormat(DataFormatString = "{0} 歳")]
         public int Age { get; set; }
     }
 }
